9f1da310923ca048e752c5f2a759debea727221f
SKishchenko <userg@yandex.ru>
2024-09-24 12:10:21 +0300

    add implemented

--- src/main/java/Phone/Main.java
+++ src/main/java/Phone/Main.java
@@ -1,7 +1,9 @@
 package Phone;
 
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
     }
 }
--- src/main/java/Phone/PhoneBook.java
+++ src/main/java/Phone/PhoneBook.java
@@ -7,7 +7,15 @@ public class PhoneBook {
 
     private Map<String, String> contacts = new HashMap<>();
 
-    public int add(){
-        return 0;
+    public Map<String, String> getContacts() {
+        return contacts;
     }
+
+    public int add(String name, String phoneNumber) {
+        if (!contacts.containsKey(name)) {
+            contacts.put(name, phoneNumber);
+        }
+        return contacts.size();
+    }
+
 }
